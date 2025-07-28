9bbab4369706c3d8c8d32022b17a9ab7706eb219
Michelle Glebova <147517074+michlleee@users.noreply.github.com>
2025-07-28 14:12:40 +0700

    Reinitialize project folder
    Fix bug tailwind

---
+++ trash-tracker/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen bg-blue-600 flex items-center justify-center">
+        <h1 className="text-white font-bold text-4xl">Tailwind is working</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
